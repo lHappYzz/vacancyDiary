73fbf82d3eab1cea39d1ebce5c3c9555dcbc89ed
Martynenko Oleksandr <48642804+lHappYzz@users.noreply.github.com>
2021-02-18 00:23:45 +0200

    first commit

---
+++ app/Http/Controllers/VacancyController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Vacancy;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+
+class VacancyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return View
+     */
+    public function index()
+    {
+        return view('pages.vacancies.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        dd('create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Vacancy  $vacancy
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Vacancy $vacancy)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Vacancy  $vacancy
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Vacancy $vacancy)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Vacancy  $vacancy
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Vacancy $vacancy)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Vacancy  $vacancy
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Vacancy $vacancy)
+    {
+        //
+    }
+}
---
+++ resources/views/pages/vacancies/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('title', 'Vacancies')
+
+@section('content')
+    <div class="test">
+        <p>Hello from vacancies index</p>
+    </div>
+@endsection
