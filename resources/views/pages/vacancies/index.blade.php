dd59a3a636f6a9850eb060e922376ecd4eb2a76a
Martynenko Oleksandr <48642804+lHappYzz@users.noreply.github.com>
2021-02-24 10:10:32 +0200

    vacancy index page styles exported to file at css folder

--- resources/views/pages/vacancies/index.blade.php
+++ resources/views/pages/vacancies/index.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app')
 @section('title', 'Vacancies')
-
+@push('styles')
+    <link rel="stylesheet" href="{{ asset('public/css/vacancyIndex.css') }}?v={{ filemtime('public/css/vacancyIndex.css') }}">
+@endpush
 @section('content')
 
     @if(session('message'))
@@ -11,13 +13,13 @@
 
     <div class="wrapper d-flex flex-wrap ">
         @forelse($vacancies as $vacancy)
-            <div class="card m-2 text-center" style="width: 100%; max-width: 20rem">
+            <div class="card m-2 text-center">
                 <div class="card-body" style="color: {{ $vacancy->status->hex_color }};">
                     <p class="card-title">
-                        <small style="background-color: white; position: absolute; right: 1rem; top: -0.9em; color: {{$vacancy->status->hex_color}}">
+                        <span class="card-label" style="color: {{$vacancy->status->hex_color}}">
                             {{ $vacancy->status->name }}
                             {{ $vacancy->status_assigned_at->diffForHumans() }}
-                        </small>
+                        </span>
                         <a target="_blank" style="color: inherit" class="link" href="{{ $vacancy->link }}">{{ $vacancy->position }}</a>
                         <small class="fs-6 text-secondary">at&nbsp{{ $vacancy->company_name }}</small>
                     </p>
@@ -26,11 +28,11 @@
                     </p>
                     <div class="row">
                         <div class="col">
-                            <a href="{{ route('vacancy.edit', ['vacancy' => $vacancy->id]) }}" class="card-link btn btn-outline-primary mb-2" style="width: 100%">Edit status</a>
+                            <a href="{{ route('vacancy.edit', ['vacancy' => $vacancy->id]) }}" class="card-link btn btn-outline-primary mb-2">Edit status</a>
                             <form id="deleteVacancy" action="{{ route('vacancy.destroy', ['vacancy' => $vacancy->id]) }}" method="post">
                                 @csrf
                                 @method('delete')
-                                <button class="card-link btn btn-outline-danger" onclick="document.closest('form#deleteVacancy').submit()" style="width: 100%">Delete</button>
+                                <button class="card-link btn btn-outline-danger" onclick="document.closest('form#deleteVacancy').submit()">Delete</button>
                             </form>
                         </div>
                     </div>
